d299e5d0aa54653828ccd0e7789dde3f435ced5f
VIVI <15325061080293@cetis108.edu.mx>
2017-06-07 14:18:07 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/cetis108/cetis1084am_15325061080293_app005/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.edu.cetis108.cetis1084am_15325061080293_app005;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
