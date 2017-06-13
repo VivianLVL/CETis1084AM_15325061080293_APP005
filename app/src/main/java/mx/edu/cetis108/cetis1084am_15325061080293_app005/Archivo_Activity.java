d3c852a3a19da233b63d11aff1e0ea8f04bc4b52
VIVI <15325061080293@cetis108.edu.mx>
2017-06-13 09:22:12 -0600

    commit3

--- app/src/main/java/mx/edu/cetis108/cetis1084am_15325061080293_app005/Archivo_Activity.java
+++ app/src/main/java/mx/edu/cetis108/cetis1084am_15325061080293_app005/Archivo_Activity.java
@@ -3,6 +3,7 @@ package mx.edu.cetis108.cetis1084am_15325061080293_app005;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.StaticLayout;
+import android.widget.EditText;
 
 public class Archivo_Activity extends AppCompatActivity {
 
@@ -10,22 +11,38 @@ public class Archivo_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivo_);
-
     }
+
+    EditText txtNombre;
+    EditText txtTipo;
+    EditText txtRuta;
+    EditText txtTexto;
 
     String nombre;
     String tipo;
     String ruta;
     String texto;
 
-
-
-    void Archivo_Activity(String Vnombre, String Vtipo, String Vruta, String Vtexto)
+    public Archivo_Activity(String Vnombre, String Vtipo, String Vruta, String Vtexto)
     {
         nombre=Vnombre;
         tipo=Vtipo;
         ruta=Vruta;
         texto=Vtexto;
+
+        txtNombre = (EditText)findViewById(R.id.txtNombre);
+        txtNombre.setText(nombre);
+
+        txtTipo=(EditText)findViewById(R.id.txtTipo);
+        txtTipo.setText(tipo);
+
+        txtRuta=(EditText)findViewById(R.id.txtRuta);
+        txtRuta.setText(ruta);
+
+        txtTexto=(EditText)findViewById(R.id.txtTexto);
+        txtTexto.setText(texto);
+
+
 
     }
 
